cfef252461847dc78d9622f72f93a2f5494a5f54
linfeng <1925466036@qq.com>
2022-08-10 23:36:01 +0800

    modify some code about linker.ld

--- loongrCore/src/test/mod.rs
+++ loongrCore/src/test/mod.rs
@@ -2,30 +2,24 @@ use crate::{DEBUG, ERROR, INFO,TRACE, WARN};
 
 pub fn color_output_test() {
     extern "C" {
-        fn _text_start();
-        fn _text_end();
-        fn _rodata_start();
-        fn _rodata_end();
-        fn _data_start();
-        fn _data_end();
-        fn _bss_start();
-        fn _bss_end();
-        fn _stack();
-        fn _heap_start();
-        fn _heap_size();
+        fn text_start();
+        fn text_end();
+        fn rodata_start();
+        fn rodata_end();
+        fn data_start();
+        fn data_end();
+        fn bss_start();
+        fn bss_end();
+        fn stack();
+        fn heap_start();
+        fn heap_size();
     }
-    WARN!(".text [{:#x}, {:#x})", _text_start as usize, _text_end as usize);
-    INFO!(".rodata [{:#x}, {:#x})", _rodata_start as usize, _rodata_end as usize);
-    DEBUG!(".data [{:#x}, {:#x})", _data_start as usize, _data_end as usize);
+    WARN!(".text [{:#x}, {:#x})", text_start as usize, text_end as usize);
+    INFO!(".rodata [{:#x}, {:#x})", rodata_start as usize, rodata_end as usize);
+    DEBUG!(".data [{:#x}, {:#x})", data_start as usize, data_end as usize);
     TRACE!(
         ".bss [{:#x}, {:#x})",
-        _bss_start as usize,
-        _bss_end as usize
+        bss_start as usize,
+        bss_end as usize
     );
-    TRACE!(
-        "heap [{:#x}, {:#x})",
-        _heap_start as usize,
-        _heap_start as usize + _heap_size as usize
-    );
-    TRACE!("stack [{:#x}, {:#x})", _bss_end as usize, _stack as usize);
 }
